1cfb21ba632a7fde8d4de1549c331f470b39ec6c
patrick <pross.go@gmail.com>
2022-10-12 18:18:24 +0300

    task1_1

--- HW/Seminar_1/task1.py
+++ HW/Seminar_1/task1.py
@@ -1,1 +1,7 @@
-print ('Hello')
+day = int(input ('Введите день недели цифрой от 1 до 7'))
+if day < 1 or day > 7:
+    print ('Вы ввели некорректную цифру')
+elif day == 6 or day ==7:
+    print ('Да')
+else:
+    print ('Нет')
